6a010c355f5cd634c8f561ea8ae7150bd2ba9f4e
yooseong12 <rladbtjd81@gmail.com>
2022-09-21 11:45:17 +0900

    file DTO class 추가

--- src/main/java/com/board/configuration/MvcConfiguration.java
+++ src/main/java/com/board/configuration/MvcConfiguration.java
@@ -1,6 +1,8 @@
 package com.board.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,6 +15,15 @@ public class MvcConfiguration implements WebMvcConfigurer {
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(new LoggerInterceptor())
 		.excludePathPatterns("/css/**", "/fonts/**", "/plugin/**", "/scripts/**");
+	}
+
+	@Bean
+	public CommonsMultipartResolver multipartResolver(){
+		CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+		multipartResolver.setDefaultEncoding("UTF-8");
+		multipartResolver.setMaxUploadSizePerFile(5 * 1024 * 1024);
+
+		return multipartResolver;
 	}
 
 }
