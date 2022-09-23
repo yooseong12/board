9f0ad9fdd274529de1b541d1eaa0f900c5b1ec18
yooseong12 <rladbtjd81@gmail.com>
2022-09-23 14:44:19 +0900

    로그인 관련 로직 추가

---
+++ src/main/java/com/board/controller/UserController.java
@@ -0,0 +1,46 @@
+package com.board.controller;
+
+import com.board.configuration.security.JwtTokenProvider;
+import com.board.domain.user.UserRepository;
+import com.board.domain.user.Users;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
+
+
+@RequiredArgsConstructor
+@RestController
+public class UserController {
+
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final UserRepository userRepository;
+
+    // 회원가입
+    @PostMapping("/join")
+    public Long join(@RequestBody Map<String, String> user){
+        return userRepository.save(Users.builder()
+                .email(user.get("email"))
+                .password(passwordEncoder.encode(user.get("password")))
+                        .roles(Collections.singletonList("ROLE_USER")) // 최초 가입시 USER 로 설정
+                .build()).getId();
+    }
+
+    //로그인
+    @PostMapping("/login")
+    public String login(@RequestBody Map<String, String> user){
+        Users member = userRepository.findByEmail(user.get("email"))
+                .orElseThrow(() -> new IllegalArgumentException("가입되지 않은 E-MAIL 입니다."));
+        if (!passwordEncoder.matches(user.get("password"), member.getPassword())){
+            throw new IllegalArgumentException("잘못된 비밀번호입니다.");
+        }
+        return jwtTokenProvider.createToken(member.getUsername(),member.getRoles());
+    }
+
+
+}
